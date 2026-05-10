ac703e6084398fd50fa3ed9586808435dbcbd80c
4upak <serg.chupak@gmail.com>
2026-05-10 19:04:40 +0000

    комміт з девконтейнера

--- homework_04/build/CMakeFiles/ugv_odometry.dir/DependInfo.cmake
+++ homework_04/build/CMakeFiles/ugv_odometry.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/anna/miltech/homework_04/src/main.cpp" "CMakeFiles/ugv_odometry.dir/src/main.cpp.o" "gcc" "CMakeFiles/ugv_odometry.dir/src/main.cpp.o.d"
+  "/workspaces/miltech/homework_04/src/main.cpp" "CMakeFiles/ugv_odometry.dir/src/main.cpp.o" "gcc" "CMakeFiles/ugv_odometry.dir/src/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
